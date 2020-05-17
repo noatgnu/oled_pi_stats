ffa802b9b25f3bc7adf9fa940ad5e04359399fd5
Toan Phung <toan.phung@uqconnect.edu.au>
2020-05-17 13:22:36 +1000

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ while True:
     # Write two lines of text.
 
     draw.text((x, top), "IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 8), CPU + " / Up:" + boot_uptime_str, font=font, fill=255)
+    draw.text((x, top + 8), CPU + "/Up:" + boot_uptime_str, font=font, fill=255)
     draw.text((x, top + 16), MemUsage, font=font, fill=255)
     draw.text((x, top + 25), Disk, font=font, fill=255)
     #draw.text((x, top + 28), boot_uptime_str, font=font, fill=255)
